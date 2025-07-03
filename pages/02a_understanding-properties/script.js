32e640757d8196f57389e66aad63f2f0cfbcede1
Israel Effum <effumprince345@gmail.com>
2025-07-03 13:11:10 +0000

    Update script.js

---
+++ pages/02a_understanding-properties/script.js
@@ -0,0 +1,30 @@
+import gsap from 'gsap';
+
+
+
+
+gsap.to('.box',{
+ y: -50,
+ repeat: -1,
+ duration: 1.2,
+ yoyo: true,
+}
+
+
+
+);
+
+gsap.to('.box2', {
+   y: -50,
+   repeat: -1,
+   duration: 1,
+   yoyo: true,
+})
+
+
+gsap.to('.box3', {
+   y: -50,
+   repeat: -1,
+   duration: 0.8,
+   yoyo: true,
+}),
