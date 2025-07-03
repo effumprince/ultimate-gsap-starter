e436d2723c90cc3a1af9787c747652095a50f5d4
Israel Effum <effumprince345@gmail.com>
2025-07-03 13:13:52 +0000

    Update script.js

---
+++ pages/01b_challenge/script.js
@@ -0,0 +1,15 @@
+gsap.fromTo('.card',
+    {
+        x: 400,
+        y: -100,
+        opacity: 0,
+        // duration:3,
+    },
+    {
+        x: -400,
+        y: 300,
+        opacity:1,
+        duration: 3,
+      scale: 2,
+    }
+)
